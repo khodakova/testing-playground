059c0a92402861022ced51cf9731ff65b91a5271
khodakova <khodakova.v@gmail.com>
2024-12-20 23:17:56 +0300

    init

---
+++ packages/app/src/app/index.ts
@@ -0,0 +1,3 @@
+export {default as App} from './app.vue';
+export {router} from './providers/router-provider/router-provider.ts'
+
---
+++ packages/app/src/main.ts
@@ -0,0 +1,10 @@
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import { App, router } from '@/app'
+
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router)
+
+app.mount('#app')
